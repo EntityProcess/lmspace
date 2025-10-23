c52ce8887a1afd74e96dd29f27ebc30979e5e4fa
Christopher Tso <christso@gmail.com>
2025-10-23 16:28:09 +1100

    remove transpiler

--- src/lmspace/vscode/__init__.py
+++ src/lmspace/vscode/__init__.py
@@ -4,10 +4,8 @@ from __future__ import annotations
 
 from .launch_agent import launch_agent
 from .provision import provision_subagents
-from .transpiler import transpile_skill
 
 __all__ = [
     "launch_agent",
     "provision_subagents",
-    "transpile_skill",
 ]
